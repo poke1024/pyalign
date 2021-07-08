d0aaa34c72f01e884c83fe34d0cee455686ae54c
Bernhard Liebl <poke1024@gmx.de>
2021-07-08 13:57:56 +0200

    wip build wheels

--- scripts/prepare_for_build_macos.sh
+++ scripts/prepare_for_build_macos.sh
@@ -7,3 +7,4 @@ python scripts/install_xtensor.py xtl 0.7.2
 python scripts/install_xtensor.py xtensor 0.23.4
 python scripts/install_xtensor.py xsimd 7.4.9
 python scripts/install_xtensor.py xtensor-python 0.25.1
+export MACOSX_DEPLOYMENT_TARGET=10.14
