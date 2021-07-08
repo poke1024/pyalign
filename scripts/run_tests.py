eb8c7938cedf2173865fcf631bc002967ac93726
Bernhard Liebl <poke1024@gmx.de>
2021-07-08 16:44:35 +0200

    wip

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,6 +1,21 @@
 import nose
 import os
+import sys
+import traceback
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-nose.main(defaultTest="pyalign.tests", config=nose.config.Config(verbosity=3))
+try:
+	success = nose.run(
+		defaultTest="pyalign.tests",
+		config=nose.config.Config(verbosity=3))
+except:
+	traceback.print_exc()
+	raise
+
+if success:
+	print("tests ok.")
+	sys.exit(0)
+else:
+	print("tests failed.")
+	sys.exit(1)
