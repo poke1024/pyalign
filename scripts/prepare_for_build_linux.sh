78a904675b9b3b09ab19fc62356b99639f59cbec
Bernhard Liebl <poke1024@gmx.de>
2021-07-09 04:59:51 +0200

    wip

--- scripts/prepare_for_build_linux.sh
+++ scripts/prepare_for_build_linux.sh
@@ -1,5 +1,5 @@
-yum install -y devtoolset-9-toolchain
-scl enable devtoolset-9 bash
+#yum install -y devtoolset-9-toolchain
+#scl enable devtoolset-9 bash
 cc --version
 yum install -y git cmake
 scripts/prepare_for_build.sh
---
+++ scripts/prepare_for_build_win.sh
@@ -0,0 +1,6 @@
+choco install python --version 3.8.6
+export PATH="/c/Python38:/c/Python38/Scripts:$PATH"
+ln -s /c/Python38/python.exe /c/Python38/python3.exe
+choco install llvm
+cc --version
+choco install git cmake
