adc676e9c25fd6ae3231261fe6d312eb452dfa1f
Bernhard Liebl <poke1024@gmx.de>
2021-07-09 05:38:23 +0200

    wip

--- scripts/prepare_for_build_win.sh
+++ scripts/prepare_for_build_win.sh
@@ -1,10 +1,10 @@
 choco install python --version 3.8.6
 export PATH="/c/Python38:/c/Python38/Scripts:$PATH"
 ln -s /c/Python38/python.exe /c/Python38/python3.exe
-choco install llvm
+ln -s /c/Python38/Scripts/pip3.exe /c/Python38/Scripts/pip.exe
 cc --version
 choco install git cmake
-pip install "pybind11[global]~=2.6.2" "numpy~=1.19"
+pip install "pybind11~=2.6.2" "pybind11[global]~=2.6.2" "numpy~=1.19"
 python scripts/install_xtensor.py xtl 0.7.2
 python scripts/install_xtensor.py xtensor 0.23.10
 python scripts/install_xtensor.py xsimd 7.4.9
