90b41b15f36be1a5a33e02b51ae15b871b154d87
Bernhard Liebl <poke1024@gmx.de>
2021-07-09 10:38:46 +0200

    wip

--- scripts/prepare_for_build_win.sh
+++ scripts/prepare_for_build_win.sh
@@ -1,9 +1,10 @@
-choco install python --version 3.8.6 --no-progress
-export PATH="/c/Python38:/c/Python38/Scripts:$PATH"
+#choco install python --version 3.8.6 --no-progress
+#export PATH="/c/Python38:/c/Python38/Scripts:$PATH"
 cc --version
 choco install git cmake
-python -m ensurepip
-python -m pip install "pybind11[global]~=2.6.2" "numpy~=1.19"
+#python -m ensurepip
+#python -m pip install "pybind11[global]~=2.6.2" "numpy~=1.19"
+pip "pybind11[global]~=2.6.2" "numpy~=1.19"
 python scripts/install_xtensor.py xtl 0.7.2
 python scripts/install_xtensor.py xtensor 0.23.10
 python scripts/install_xtensor.py xsimd 7.4.9
