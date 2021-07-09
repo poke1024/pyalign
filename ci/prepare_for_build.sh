77c445be3a205828c3c541f4ee8084f91f806087
Bernhard Liebl <poke1024@gmx.de>
2021-07-09 19:50:09 +0200

    wip

--- ci/prepare_for_build.sh
+++ ci/prepare_for_build.sh
@@ -1,9 +1,9 @@
 # specifing a wrong numpy version here will end up in runtime errors like:
 # module compiled against API version 0xe but this version of numpy is 0xd
-python -m ensurepip
-python -m pip install wheel
-python -m pip install pip install "pybind11[global]~=2.6.2" "numpy~=1.19"
-python ci/install_xtensor.py xtl 0.7.2
-python ci/install_xtensor.py xsimd 7.4.9
-python ci/install_xtensor.py xtensor 0.23.10
-python ci/install_xtensor.py xtensor-python 0.25.3
+python3 -m ensurepip
+python3 -m pip install wheel
+python3 -m pip install pip install "pybind11[global]~=2.6.2" "numpy~=1.19"
+python3 ci/install_xtensor.py xtl 0.7.2
+python3 ci/install_xtensor.py xsimd 7.4.9
+python3 ci/install_xtensor.py xtensor 0.23.10
+python3 ci/install_xtensor.py xtensor-python 0.25.3
--- ci/prepare_for_build_win.sh
+++ ci/prepare_for_build_win.sh
@@ -2,11 +2,11 @@
 #export PATH="/c/Python38:/c/Python38/Scripts:$PATH"
 cc --version
 choco install git cmake
-python -m ensurepip
-python -m pip install wheel
-python -m pip install --only-binary :all: "numpy~=1.19"
-python -m pip install "pybind11[global]~=2.6.2"
-python ci/install_xtensor.py xtl 0.7.2
-python ci/install_xtensor.py xsimd 7.4.9
-python ci/install_xtensor.py xtensor 0.23.10
-python ci/install_xtensor.py xtensor-python 0.25.3
+python3 -m ensurepip
+python3 -m pip install wheel
+python3 -m pip install --only-binary :all: "numpy~=1.19"
+python3 -m pip install "pybind11[global]~=2.6.2"
+python3 ci/install_xtensor.py xtl 0.7.2
+python3 ci/install_xtensor.py xsimd 7.4.9
+python3 ci/install_xtensor.py xtensor 0.23.10
+python3 ci/install_xtensor.py xtensor-python 0.25.3
