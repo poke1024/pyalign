d77e70760c5610f04183394b57af2891f4789c88
Bernhard Liebl <poke1024@gmx.de>
2021-07-29 20:33:00 +0200

    add "local" specifier to pybind classes

--- pyalign/algorithm/pyalign.h
+++ pyalign/algorithm/pyalign.h
@@ -21,7 +21,7 @@ void register_solver(py::module_ &m) {
 
 	m.def("create_solver", &create_solver<Options>);
 
-	py::class_<Solver<Value, Index>, SolverRef<Value, Index>> solver(m, "Solver");
+	py::class_<Solver<Value, Index>, SolverRef<Value, Index>> solver(m, "Solver", py::module_local());
 	solver.def_property_readonly("options", &Solver<Value, Index>::options);
 	solver.def_property_readonly("batch_size", &Solver<Value, Index>::batch_size);
 	solver.def("solve_for_score", &Solver<Value, Index>::solve_for_score);
@@ -35,15 +35,17 @@ void register_solver(py::module_ &m) {
 	solver.def("solve_indexed_for_solution", &Solver<Value, Index>::solve_indexed_for_solution);
 	solver.def("solve_indexed_for_solution_iterator", &Solver<Value, Index>::solve_indexed_for_solution_iterator);
 
-	py::class_<Alignment<Index>, AlignmentRef<Index>> alignment(m, "Alignment");
+	py::class_<Alignment<Index>, AlignmentRef<Index>> alignment(
+		m, "Alignment", py::module_local());
 	alignment.def_property_readonly("score", &Alignment<Index>::score);
 	alignment.def_property_readonly("s_to_t", &Alignment<Index>::s_to_t);
 	alignment.def_property_readonly("t_to_s", &Alignment<Index>::t_to_s);
 
-	py::class_<AlignmentIterator<Index>, AlignmentIteratorRef<Index>> alignment_iterator(m, "AlignmentIterator");
+	py::class_<AlignmentIterator<Index>, AlignmentIteratorRef<Index>> alignment_iterator(
+		m, "AlignmentIterator", py::module_local());
 	alignment_iterator.def("next", &AlignmentIterator<Index>::next);
 
-	py::class_<Solution, SolutionRef> solution(m, "Solution");
+	py::class_<Solution, SolutionRef> solution(m, "Solution", py::module_local());
 	solution.def_property_readonly("values", &Solution::values);
 	solution.def_property_readonly("traceback_has_max_degree_1", &Solution::traceback_has_max_degree_1);
 	solution.def_property_readonly("traceback_as_matrix", &Solution::traceback_as_matrix);
@@ -53,15 +55,18 @@ void register_solver(py::module_ &m) {
 	solution.def_property_readonly("alignment", &Solution::alignment);
 	solution.def_property_readonly("algorithm", &Solution::algorithm);
 
-	py::class_<SolutionIterator, SolutionIteratorRef> solution_iterator(m, "SolutionIterator");
+	py::class_<SolutionIterator, SolutionIteratorRef> solution_iterator(
+		m, "SolutionIterator", py::module_local());
 	solution_iterator.def("next", &SolutionIterator::next);
 
-	py::class_<Algorithm, AlgorithmRef> algorithm(m, "Algorithm");
+	py::class_<Algorithm, AlgorithmRef> algorithm(
+		m, "Algorithm", py::module_local());
 	algorithm.def_property_readonly("name", &Algorithm::name);
 	algorithm.def_property_readonly("runtime", &Algorithm::runtime);
 	algorithm.def_property_readonly("memory", &Algorithm::memory);
 
-	py::class_<Options, std::shared_ptr<Options>> options(m, "Options");
+	py::class_<Options, std::shared_ptr<Options>> options(
+		m, "Options", py::module_local());
 	m.def("create_options", &create_options<Options>);
 }
 
